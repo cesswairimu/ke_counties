7959727d4ab84a961e424d5d44bb079e91622236
Cess <cessmbuguar@gmail.com>
2024-04-04 20:06:02 +0300

    tests for methods

--- spec/counties_spec.rb
+++ spec/counties_spec.rb
@@ -6,5 +6,26 @@ RSpec.describe KeCounties::Counties do
   it 'encapsulates an internal hash' do
     expect(described_class::HASH).to be_a(Hash)
   end
+
+  describe '.content' do
+    subject { described_class.content }
+    let(:sample) { subject.first }
+
+    it { is_expected.to be_a(Array) }
+    it { expect(subject.size).to eq(47) }
+    
+    it { expect(sample).to be_a(Hash)  }
+    it { expect(sample).to include(:code) }
+    it { expect(sample).to include(:abbrv) }
+    it { expect(sample).to include(:capital) }
+    it { expect(sample).to include(:province)}
+  end
+
+  describe '.names' do
+    subject { described_class.names }
+
+    it { expect(subject).to include('Uasin Gishu') }
+    it { expect(subject.size).to eq(47) }
+  end
   
 end
--- spec/ke_counties_spec.rb
+++ spec/ke_counties_spec.rb
@@ -7,8 +7,35 @@ RSpec.describe KeCounties do
     expect(described_class::VERSION).not_to be nil  
   end
 
-  # it 'encapsulates an internal hash' do
-  #   expect(described_class::HASH).to raise_error(NameError)
-  # end
+  describe '.codes' do
+    subject {described_class.codes} 
+
+    it { is_expected.to be_a(Array) }
+    it { expect(subject.size).to eq(47) }
+
+    it { expect(subject).to include("034")  }
+    it { expect(subject).not_to include("048") }
+  end
+
+  describe '.content' do
+    subject { described_class.content }
+    let(:sample) { subject.first }
+
+    it { is_expected.to be_a(Array) }
+    it { expect(subject.size).to eq(47) }
+    
+    it { expect(sample).to be_a(Hash)  }
+    it { expect(sample).to include(:code) }
+    it { expect(sample).to include(:abbrv) }
+    it { expect(sample).to include(:capital) }
+    it { expect(sample).to include(:province)}
+  end
+
+  describe '.names' do
+    subject { described_class.names }
+
+    it { expect(subject).to include('Uasin Gishu') }
+    it { expect(subject.size).to eq(47) }
+  end
   
 end
